e849e06ca991e5a49c7f63e2264d7083c25c0598
Readyornot69 <elianeartesa4567@gmail.com>
2025-03-27 12:05:11 -0300

    Create Opengl_support.lua

---
+++ Opengl_support.lua
@@ -0,0 +1,16 @@
+-- OpenGL support for rendering in Redtooth
+
+local opengl = {}
+
+-- Set up OpenGL for video streaming and rendering
+function opengl.setup()
+    -- OpenGL setup code for rendering video, UI, etc.
+    return true
+end
+
+-- Render OpenGL graphics for streaming or UI
+function opengl.render()
+    -- Handle OpenGL rendering logic
+end
+
+return opengl
